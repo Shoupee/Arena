15fc13558f8afc089788929558ceac36285beccb
Shoupee <sharif.amir120@gmail.com>
2025-10-22 21:44:49 +0330

    Create linera-init.js

---
+++ linera-init.js
@@ -0,0 +1,9 @@
+// linera-init.js
+// This file will host the real Linera Testnet integration in the next step.
+// For now, we just expose a placeholder object and a visible console message.
+
+window.Linera = {
+  status: "booting",
+};
+
+console.log("Linera: init placeholder loaded.");
